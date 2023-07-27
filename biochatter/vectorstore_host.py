e4a1643b0aa5a59dca69b1c9cd78d6065ef8435a
Sebastian Lobentanzer <sebastian.lobentanzer@gmail.com>
2023-07-27 11:13:22 +0200

    formatting

--- biochatter/vectorstore_host.py
+++ biochatter/vectorstore_host.py
@@ -2,12 +2,19 @@ import logging
 from typing import List, Optional, Tuple, Dict
 import re
 import base64
-from pymilvus import MilvusException, connections, Collection, utility, FieldSchema
+from pymilvus import (
+    MilvusException,
+    connections,
+    Collection,
+    utility,
+    FieldSchema,
+)
 from langchain.vectorstores import Milvus
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.schema import Document
 
 logger = logging.getLogger(__name__)
+
 
 def _grab_text_field_name(fields: List[FieldSchema]) -> str | None:
     prev_field = None
@@ -17,31 +24,36 @@ def _grab_text_field_name(fields: List[FieldSchema]) -> str | None:
         prev_field = x
     return None
 
+
 def extract_from_alias(alias: str) -> Optional[Tuple]:
     # valid alias is {base64}_c{uuid4.hex}
     pattern = r"^([0-9a-zA-Z_]+)_c([a-f0-9]{32})$"
     match = re.fullmatch(pattern, alias)
     return None if not match else match.groups()
 
+
 def string_to_base64(txt: str) -> str:
-    '''
+    """
     As collection alias only supports numbers, letters and underscores, we have
     to encode document name to url base64 string, and encode "-" and "=" of base64
     string to "a_a" and "b_b"
-    '''
+    """
     try:
-        converted = base64.urlsafe_b64encode(txt.encode("utf-8")).decode("utf-8")
-        return converted.replace('-', 'a_a').replace("=", "b_b")
+        converted = base64.urlsafe_b64encode(txt.encode("utf-8")).decode(
+            "utf-8"
+        )
+        return converted.replace("-", "a_a").replace("=", "b_b")
     except Exception as e:
         logger.error(e)
         raise e
 
+
 def base64_to_string(b64_txt: str) -> str:
-    '''
+    """
     Decode base64 string to document name
-    '''
+    """
     try:
-        converted = b64_txt.replace('a_a', '-').replace("b_b", "=")
+        converted = b64_txt.replace("a_a", "-").replace("b_b", "=")
         return base64.urlsafe_b64decode(converted).decode("utf-8")
     except Exception as e:
         logger.error(e)
@@ -50,20 +62,18 @@ def base64_to_string(b64_txt: str) -> str:
 
 class VectorCollection:
     def __init__(
-            self,
-            col_name: str,
-            doc_name: str,
-            text_field: Optional[str]="text"
-        ) -> None:
+        self, col_name: str, doc_name: str, text_field: Optional[str] = "text"
+    ) -> None:
         self.collection_name = col_name
         self.document_name = doc_name
         self.text_field = text_field
 
+
 class VectorDatabaseHostMilvus:
     def __init__(
         self,
-        embeddings: Optional[OpenAIEmbeddings]=None,
-        connection_args: Optional[dict]=None
+        embeddings: Optional[OpenAIEmbeddings] = None,
+        connection_args: Optional[dict] = None,
     ):
         self._collections: List[VectorCollection] = []
         self._embeddings: Optional[OpenAIEmbeddings] = embeddings
@@ -82,14 +92,14 @@ class VectorDatabaseHostMilvus:
         except MilvusException as e:
             logger.error(f"Failed to create connection to {host}:{port}")
             raise e
-        
+
         self._collections = []
         collections = utility.list_collections()
         for col_name in collections:
             col = Collection(col_name)
             if len(col.aliases) == 0:
                 continue
-            
+
             for alias in col.aliases:
                 matched_grp = extract_from_alias(alias)
                 if not matched_grp:
@@ -103,38 +113,45 @@ class VectorDatabaseHostMilvus:
                     VectorCollection(
                         col_name=col_name,
                         doc_name=docname,
-                        text_field=text_field
+                        text_field=text_field,
                     )
-                )                    
+                )
                 break
-    
+
     @property
     def collections(self) -> List[Dict[str, str]]:
         return [
-            {"document_name": col.document_name, "collection_name": col.collection_name}
+            {
+                "document_name": col.document_name,
+                "collection_name": col.collection_name,
+            }
             for col in self._collections
         ]
 
-    def _find_vector_collection(self, collection_name: str) -> Optional[VectorCollection]:
+    def _find_vector_collection(
+        self, collection_name: str
+    ) -> Optional[VectorCollection]:
         for obj in self._collections:
             if obj.collection_name == collection_name:
                 return obj
         return None
+
     def _remove_vector_collection(self, collection_name: str) -> bool:
         for obj in self._collections:
             if obj.collection_name == collection_name:
                 self._collections.remove(obj)
                 return True
         return False
+
     def store_embedding(
-            self, 
-            doc_name: str,
-            documents: List[Document],
-        ) -> Dict[str, str]:
+        self,
+        doc_name: str,
+        documents: List[Document],
+    ) -> Dict[str, str]:
         db = Milvus.from_documents(
             documents=documents,
             embedding=self._embeddings,
-            connection_args=self._connection_args
+            connection_args=self._connection_args,
         )
         encoded_doc_name = string_to_base64(doc_name)
         alias = f"{encoded_doc_name}_{db.collection_name}"
@@ -142,30 +159,32 @@ class VectorDatabaseHostMilvus:
         vector_collection = VectorCollection(
             col_name=db.collection_name,
             doc_name=doc_name,
-            text_field=db.text_field
+            text_field=db.text_field,
         )
         self._collections.append(vector_collection)
         return {
-            "document_name": doc_name, 
-            "collection_name": db.collection_name
+            "document_name": doc_name,
+            "collection_name": db.collection_name,
         }
-    
-    def similarity_search(self, collection_name: str, query: str, k: int=3) -> List[Document]:
+
+    def similarity_search(
+        self, collection_name: str, query: str, k: int = 3
+    ) -> List[Document]:
         vector_coll = self._find_vector_collection(collection_name)
         if not vector_coll:
             raise ValueError("No current collection loaded")
         try:
             db = Milvus(
-                embedding_function=self._embeddings, 
-                collection_name=collection_name, 
+                embedding_function=self._embeddings,
+                collection_name=collection_name,
                 connection_args=self._connection_args,
-                text_field=vector_coll.text_field
+                text_field=vector_coll.text_field,
             )
             return db.similarity_search(query=query, k=k)
         except MilvusException as e:
             logger.error(e)
             raise e
-    
+
     def drop_collection(self, collection_name: str) -> None:
         if not self._remove_vector_collection(collection_name):
             return
@@ -175,9 +194,3 @@ class VectorDatabaseHostMilvus:
         except MilvusException as e:
             logger.error(e)
             raise e
-
-
-
-
-                
-
--- test/test_vectorstore_host.py
+++ test/test_vectorstore_host.py
@@ -1,4 +1,3 @@
-
 import os
 import pytest
 from typing import List
@@ -9,43 +8,53 @@ from langchain.schema import Document
 from pymilvus import utility, Collection
 
 from biochatter.vectorstore import DocumentReader
-from biochatter.vectorstore_host import VectorDatabaseHostMilvus, base64_to_string, string_to_base64
+from biochatter.vectorstore_host import (
+    VectorDatabaseHostMilvus,
+    base64_to_string,
+    string_to_base64,
+)
 
-'''
+"""
 This test need OPENAI API KEY and local milvus server
-'''
+"""
 
 _HOST = "127.0.0.1"
 _PORT = "19530"
 collection_names = []
-def prepare_splitted_documents(doc_path: str, chunk_size:int=1000, chunk_overlap:int=20) -> List[Document]:
+
+
+def prepare_splitted_documents(
+    doc_path: str, chunk_size: int = 1000, chunk_overlap: int = 20
+) -> List[Document]:
     pdf_path = doc_path
     reader = DocumentReader()
     docs = reader.load_document(pdf_path)
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=chunk_size,
-        chunk_overlap = chunk_overlap,
-        separators = [" ", ",", "\n"]
+        chunk_overlap=chunk_overlap,
+        separators=[" ", ",", "\n"],
     )
     return text_splitter.split_documents(docs)
-    
+
+
 def setup_module(module):
     pdf_path = "test/bc_summary.pdf"
     splitted_docs = prepare_splitted_documents(pdf_path)
     docname = string_to_base64(os.path.basename(pdf_path))
     db = Milvus.from_documents(
-        documents=splitted_docs, 
+        documents=splitted_docs,
         embedding=OpenAIEmbeddings(),
-        connection_args={"host": _HOST, "port": _PORT}
+        connection_args={"host": _HOST, "port": _PORT},
     )
     utility.create_alias(db.collection_name, f"{docname}_{db.collection_name}")
     collection_names.append(db.collection_name)
-    
+
 
 def teardown_module(module):
     for name in collection_names:
         col = Collection(name)
         col.drop()
+
 
 def test_connect_host():
     # require local milvus server
@@ -54,6 +63,7 @@ def test_connect_host():
     collections = dbHost.collections
     assert not collections is None
     assert len(collections) > 0
+
 
 def test_store_embeddings_and_similarity_search():
     dbHost = VectorDatabaseHostMilvus(embeddings=OpenAIEmbeddings())
@@ -64,12 +74,16 @@ def test_store_embeddings_and_similarity_search():
 
     results = dbHost.similarity_search(
         collection_name=vector_collection["collection_name"],
-        query="What is Deep Counterfactual Networks?", 
-        k=3
+        query="What is Deep Counterfactual Networks?",
+        k=3,
     )
     assert len(results) > 0
-    assert all(["Deep Counterfactual Networks" in item.page_content for item in results])
+    assert all(
+        [
+            "Deep Counterfactual Networks" in item.page_content
+            for item in results
+        ]
+    )
     cnt = len(dbHost.collections)
     dbHost.drop_collection(vector_collection["collection_name"])
     assert (cnt - 1) == len(dbHost.collections)
-    
