be02a8a0ea4f52c9335fc76f55be4918213a1f68
Sebastian Lobentanzer <sebastian.lobentanzer@gmail.com>
2023-11-07 23:07:04 +0100

    add vectorstore benchmark prototype with matrix

---
+++ benchmark/test_vectorstore.py
@@ -0,0 +1,46 @@
+from biochatter.vectorstore import (
+    DocumentEmbedder,
+    DocumentReader,
+    Document,
+)
+import os
+import pytest
+
+# setup milvus connection
+if os.getenv("DEVCONTAINER"):
+    _HOST = "milvus-standalone"
+else:
+    _HOST = "127.0.0.1"
+_PORT = "19530"
+
+EMBEDDING_MODELS = [
+    "text-embedding-ada-002",
+]
+CHUNK_SIZES = [50, 1000]
+
+
+@pytest.mark.parametrize("model", EMBEDDING_MODELS)
+@pytest.mark.parametrize("chunk_size", CHUNK_SIZES)
+def test_document_summariser(model, chunk_size):
+    pdf_path = "test/bc_summary.pdf"
+    with open(pdf_path, "rb") as f:
+        doc_bytes = f.read()
+
+    reader = DocumentReader()
+    doc = reader.document_from_pdf(doc_bytes)
+
+    doc_ids = []
+    docsum = DocumentEmbedder(model=model, chunk_size=chunk_size)
+    docsum.connect(_HOST, _PORT)
+    doc_ids.append(docsum.save_document(doc))
+
+    query = "What is BioCypher?"
+    results = docsum.similarity_search(query)
+    correct = sum(["BioCypher" in result.page_content for result in results])
+
+    # delete embeddings
+    [docsum.database_host.remove_document(doc_id) for doc_id in doc_ids]
+
+    # record sum in CSV file
+    with open("benchmark/vectorstore_results.csv", "a") as f:
+        f.write(f"{model},{chunk_size},{correct}\n")
