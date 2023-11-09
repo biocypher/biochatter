4560a50dfc044c634220befcbed7a5d8d2b88597
Sebastian Lobentanzer <sebastian.lobentanzer@gmail.com>
2023-11-09 11:05:24 +0100

    scoring the tests

--- benchmark/test_prompts.py
+++ benchmark/test_prompts.py
@@ -2,34 +2,39 @@ from biochatter.prompts import BioCypherPromptEngine
 import pytest
 
 
+MODEL_NAMES = [
+    "gpt-3.5-turbo",
+    "gpt-4",
+]
+
+
 @pytest.fixture
-def ps():
+@pytest.mark.parametrize("model_name", MODEL_NAMES)
+def ps(model_name):
     return BioCypherPromptEngine(
-        schema_config_or_info_path="test/test_schema_info.yaml"
+        schema_config_or_info_path="test/test_schema_info.yaml",
+        model_name=model_name,
     )
 
 
+def calculate_test_score(vector: list[bool]):
+    score = sum(list)
+    max = len(list)
+    return f"{score}/{max}"
+
+
 def test_entity_selection(ps):
-    """
-
-    The first step is to identify the KG components that are relevant to the
-    question. Requires only a question as input, entities and relationships are
-    inferred from the schema.
-
-    "You have access to a knowledge graph that contains {entities} and
-    {relationships}. Your task is to select the ones that are relevant to
-    the user's question for subsequent use in a query. Only return the
-    entities and relationships, comma-separated, without any additional
-    text."
-
-    TODO: a couple more representative cases
-
-    """
     success = ps._select_entities(
         question="Which genes are associated with mucoviscidosis?"
     )
     assert success
-    assert ps.selected_entities == ["Gene", "Disease"]
+
+    score = []
+    score.append("Gene" in ps.selected_entities)
+    score.append("Disease" in ps.selected_entities)
+
+    with open("benchmark/biocypher_query_results.csv", "a") as f:
+        f.write(f"{ps.model_name},entities,{calculate_test_score(score)}\n")
 
 
 def test_relationship_selection(ps):
@@ -38,67 +43,42 @@ def test_relationship_selection(ps):
     success = ps._select_relationships()
     assert success
 
-    assert ps.selected_relationships == ["GeneToPhenotypeAssociation"]
-    assert "PERTURBED" in ps.selected_relationship_labels.keys()
-    assert "source" in ps.selected_relationship_labels.get("PERTURBED")
-    assert "target" in ps.selected_relationship_labels.get("PERTURBED")
-    assert "Disease" in ps.selected_relationship_labels.get("PERTURBED").get(
-        "source"
+    score = []
+    score.append(ps.selected_relationships == ["GeneToPhenotypeAssociation"])
+    score.append("PERTURBED" in ps.selected_relationship_labels.keys())
+    score.append("source" in ps.selected_relationship_labels.get("PERTURBED"))
+    score.append("target" in ps.selected_relationship_labels.get("PERTURBED"))
+    score.append(
+        "Disease"
+        in ps.selected_relationship_labels.get("PERTURBED").get("source")
     )
-    assert "Protein" in ps.selected_relationship_labels.get("PERTURBED").get(
-        "target"
+    score.append(
+        "Protein"
+        in ps.selected_relationship_labels.get("PERTURBED").get("target")
     )
+
+    with open("benchmark/biocypher_query_results.csv", "a") as f:
+        f.write(
+            f"{ps.model_name},relationships,{calculate_test_score(score)}\n"
+        )
 
 
 def test_property_selection(ps):
-    """
-
-    The second step is to identify the properties of the relevant KG components
-    to enable constraining the query. Requires the pre-selected entities and
-    relationships as input. The question is reused from the entity selection
-    (but can optionally be provided), and the properties are inferred from the
-    schema.
-
-    "You have access to a knowledge graph that contains {entities} and
-    {relationships}. They have the following properties: {property dictionary}.
-    Your task is to select the properties that are relevant to the user's
-    question for subsequent use in a query. Only return the entities and
-    relationships and relevant properties in dictionary format, similar to the
-    example above, without any additional text."
-
-    """
     ps.question = "Which genes are associated with mucoviscidosis?"
     ps.selected_entities = ["Gene", "Disease"]
     ps.selected_relationships = ["GeneToPhenotypeAssociation"]
     success = ps._select_properties()
     assert success
-    assert "Disease" in ps.selected_properties.keys()
-    assert "name" in ps.selected_properties.get("Disease")
+
+    score = []
+    score.append("Disease" in ps.selected_properties.keys())
+    score.append("name" in ps.selected_properties.get("Disease"))
+
+    with open("benchmark/biocypher_query_results.csv", "a") as f:
+        f.write(f"{ps.model_name},properties,{calculate_test_score(score)}\n")
 
 
 def test_query_generation(ps):
-    """
-
-    The third step is to generate a query that will retrieve the information
-    that is relevant to the user's question from the KG. Requires a question,
-    the pre-selected entities and relationships, and the pre-selected properties
-    as input. The query language can be inferred from the dedicated extended
-    biocypher schema info file, but also given as a parameter in case only the
-    schema configuration is available.
-
-    "You have access to a knowledge graph that contains {entities} and
-    {relationships} in a {database language} database. Your task is to generate
-    a query that will retrieve the information that is relevant to the user's
-    question. Only return the query, without any additional text."
-
-    TODO: need to know verbose name of disease. should be an additional mapping
-    step to get the disease name used in the database. should probably be
-    non-LLM-based, since the list of possible matches could be very long. Use a
-    combination of GILDA and fuzzy search? dedicated synonym KG?
-
-    TODO: special case relationship as node
-
-    """
     query = ps._generate_query(
         question="Which genes are associated with mucoviscidosis?",
         entities=["Gene", "Disease"],
@@ -109,15 +89,22 @@ def test_query_generation(ps):
         query_language="Cypher",
     )
 
-    assert "MATCH" in query
-    assert "RETURN" in query
-    assert "Gene" in query
-    assert "Disease" in query
-    assert "mucoviscidosis" in query
-    assert (
-        "-[:PERTURBED]->(g:Gene)" in query or "(g:Gene)<-[:PERTURBED]-" in query
+    score = []
+    score.append("MATCH" in query)
+    score.append("RETURN" in query)
+    score.append("Gene" in query)
+    score.append("Disease" in query)
+    score.append("mucoviscidosis" in query)
+    score.append(
+        (
+            "-[:PERTURBED]->(g:Gene)" in query
+            or "(g:Gene)<-[:PERTURBED]-" in query
+        )
     )
-    assert "WHERE" in query or "{name:" in query
+    score.append("WHERE" in query or "{name:" in query)
+
+    with open("benchmark/biocypher_query_results.csv", "a") as f:
+        f.write(f"{ps.model_name},cypher query,{calculate_test_score(score)}\n")
 
 
 def test_end_to_end_query_generation(ps):
@@ -126,12 +113,19 @@ def test_end_to_end_query_generation(ps):
         query_language="Cypher",
     )
 
-    assert "MATCH" in query
-    assert "RETURN" in query
-    assert "Gene" in query
-    assert "Disease" in query
-    assert "mucoviscidosis" in query
-    assert (
-        "-[:PERTURBED]->(g:Gene)" in query or "(g:Gene)<-[:PERTURBED]-" in query
+    score = []
+    score.append("MATCH" in query)
+    score.append("RETURN" in query)
+    score.append("Gene" in query)
+    score.append("Disease" in query)
+    score.append("mucoviscidosis" in query)
+    score.append(
+        (
+            "-[:PERTURBED]->(g:Gene)" in query
+            or "(g:Gene)<-[:PERTURBED]-" in query
+        )
     )
-    assert "WHERE" in query or "{name:" in query
+    score.append("WHERE" in query or "{name:" in query)
+
+    with open("benchmark/biocypher_query_results.csv", "a") as f:
+        f.write(f"{ps.model_name},end-to-end,{calculate_test_score(score)}\n")
