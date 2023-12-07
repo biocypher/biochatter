028a3e0a9c7698dafbde6da59ad6a60c2c065740
Sebastian Lobentanzer <sebastian.lobentanzer@gmail.com>
2023-12-07 16:27:50 +0100

    reset benchmark to use non-mocked models

--- benchmark/test_biocypher_query_generation.py
+++ benchmark/test_biocypher_query_generation.py
@@ -1,4 +1,3 @@
-from unittest.mock import Mock, patch
 from biochatter.prompts import BioCypherPromptEngine
 import pytest
 from .conftest import calculate_test_score, RESULT_FILES
@@ -28,142 +27,107 @@ def prompt_engine(request):
 
 
 def test_entity_selection(prompt_engine):
-    with patch("biochatter.prompts.GptConversation") as mock_gptconv:
-        system_msg = "You have access to a knowledge graph that contains these entities: Protein, Gene, Disease. Your task is to select the ones that are relevant to the user's question for subsequent use in a query. Only return the entities, comma-separated, without any additional text. "
-        mock_gptconv.return_value.query.return_value = ["Gene,Disease", Mock(), None]
-        mock_append_system_messages = Mock()
-        mock_gptconv.return_value.append_system_message = mock_append_system_messages
-        success = prompt_engine._select_entities(
-            question="Which genes are associated with mucoviscidosis?"
+    success = prompt_engine._select_entities(
+        question="Which genes are associated with mucoviscidosis?"
+    )
+    assert success
+
+    score = []
+    score.append("Gene" in prompt_engine.selected_entities)
+    score.append("Disease" in prompt_engine.selected_entities)
+
+    with open(FILE_PATH, "a") as f:
+        f.write(
+            f"{prompt_engine.model_name},entities,{calculate_test_score(score)}\n"
         )
-        assert success
-        mock_append_system_messages.assert_called_once_with((system_msg))
-
-        score = []
-        score.append("Gene" in prompt_engine.selected_entities)
-        score.append("Disease" in prompt_engine.selected_entities)
-
-        with open(FILE_PATH, "a") as f:
-            f.write(
-                f"{prompt_engine.model_name},entities,{calculate_test_score(score)}\n"
-            )
 
 
 def test_relationship_selection(prompt_engine):
     prompt_engine.question = "Which genes are associated with mucoviscidosis?"
     prompt_engine.selected_entities = ["Gene", "Disease"]
-    with patch("biochatter.prompts.GptConversation") as mock_gptconv:
-        mock_gptconv.return_value.query.return_value = ["GeneToPhenotypeAssociation", Mock(), None]
-        mock_append_system_messages = Mock()
-        mock_gptconv.return_value.append_system_message = mock_append_system_messages
-        success = prompt_engine._select_relationships()
-        assert success
-        mock_append_system_messages.assert_called_once_with('You have access to a knowledge graph that contains these entities: Gene, Disease. Your task is to select the relationships that are relevant to the user\'s question for subsequent use in a query. Only return the relationships without their sources or targets, comma-separated, and without any additional text. Here are the possible relationships and their source and target entities: [["GeneToPhenotypeAssociation", ["Disease", "Protein"]], ["GeneToPhenotypeAssociation", ["Disease", "Gene"]]].')
+    success = prompt_engine._select_relationships()
+    assert success
 
-        score = []
-        score.append(
-            prompt_engine.selected_relationships == ["GeneToPhenotypeAssociation"]
+    score = []
+    score.append(
+        prompt_engine.selected_relationships == ["GeneToPhenotypeAssociation"]
+    )
+    score.append(
+        "PERTURBED" in prompt_engine.selected_relationship_labels.keys()
+    )
+    score.append(
+        "source" in prompt_engine.selected_relationship_labels.get("PERTURBED")
+    )
+    score.append(
+        "target" in prompt_engine.selected_relationship_labels.get("PERTURBED")
+    )
+    score.append(
+        "Disease"
+        in prompt_engine.selected_relationship_labels.get("PERTURBED").get(
+            "source"
         )
-        score.append(
-            "PERTURBED" in prompt_engine.selected_relationship_labels.keys()
+    )
+    score.append(
+        "Protein"
+        in prompt_engine.selected_relationship_labels.get("PERTURBED").get(
+            "target"
         )
-        score.append(
-            "source" in prompt_engine.selected_relationship_labels.get("PERTURBED")
+    )
+
+    with open(FILE_PATH, "a") as f:
+        f.write(
+            f"{prompt_engine.model_name},relationships,{calculate_test_score(score)}\n"
         )
-        score.append(
-            "target" in prompt_engine.selected_relationship_labels.get("PERTURBED")
-        )
-        score.append(
-            "Disease"
-            in prompt_engine.selected_relationship_labels.get("PERTURBED").get(
-                "source"
-            )
-        )
-        score.append(
-            "Protein"
-            in prompt_engine.selected_relationship_labels.get("PERTURBED").get(
-                "target"
-            )
-        )
-    
-        with open(FILE_PATH, "a") as f:
-            f.write(
-                f"{prompt_engine.model_name},relationships,{calculate_test_score(score)}\n"
-            )
 
 
 def test_property_selection(prompt_engine):
     prompt_engine.question = "Which genes are associated with mucoviscidosis?"
     prompt_engine.selected_entities = ["Gene", "Disease"]
     prompt_engine.selected_relationships = ["GeneToPhenotypeAssociation"]
-    with patch("biochatter.prompts.GptConversation") as mock_gptconv:
-        resultMsg = '''
-        {
-            "Disease":{
-                "name":"mucoviscidosis"
-            },
-            "GeneToPhenotypeAssociation":{
-                "score":null,
-                "source":null,
-                "evidence":null
-            }
-        }'''
-        mock_gptconv.return_value.query.return_value = [resultMsg, Mock(), None]
-        mock_append_system_messages = Mock()
-        mock_gptconv.return_value.append_system_message = mock_append_system_messages
-        success = prompt_engine._select_properties()
-        assert success
-        mock_append_system_messages.assert_called_once_with("You have access to a knowledge graph that contains entities and relationships. They have the following properties. Entities:{'Disease': ['name', 'ICD10', 'DSM5']}, Relationships: {'GeneToPhenotypeAssociation': ['score', 'source', 'evidence']}. Your task is to select the properties that are relevant to the user's question for subsequent use in a query. Only return the entities and relationships with their relevant properties in JSON format, without any additional text. Return the entities/relationships as top-level dictionary keys, and their properties as dictionary values. Do not return properties that are not relevant to the question.")
+    success = prompt_engine._select_properties()
+    assert success
 
-        score = []
-        score.append("Disease" in prompt_engine.selected_properties.keys())
-        score.append("name" in prompt_engine.selected_properties.get("Disease"))
+    score = []
+    score.append("Disease" in prompt_engine.selected_properties.keys())
+    score.append("name" in prompt_engine.selected_properties.get("Disease"))
 
-        with open(FILE_PATH, "a") as f:
-            f.write(
-                f"{prompt_engine.model_name},properties,{calculate_test_score(score)}\n"
-            )
+    with open(FILE_PATH, "a") as f:
+        f.write(
+            f"{prompt_engine.model_name},properties,{calculate_test_score(score)}\n"
+        )
 
 
 def test_query_generation(prompt_engine):
-    with patch("biochatter.prompts.GptConversation") as mock_gptconv:
-        resultMsg = '''
-        MATCH (d:Disease {name: 'mucoviscidosis'})-[:PERTURBED]->(g:Gene)
-        RETURN g.name AS AssociatedGenes
-        '''
-        mock_gptconv.return_value.query.return_value = [resultMsg, Mock(), None]
-        mock_append_system_messages = Mock()
-        mock_gptconv.return_value.append_system_message = mock_append_system_messages
-        query = prompt_engine._generate_query(
-            question="Which genes are associated with mucoviscidosis?",
-            entities=["Gene", "Disease"],
-            relationships={
-                "PERTURBED": {"source": "Disease", "target": ["Protein", "Gene"]}
-            },
-            properties={"Disease": ["name", "ICD10", "DSM5"]},
-            query_language="Cypher",
-        )
-        mock_append_system_messages.assert_called_once_with("Generate a database query in Cypher that answers the user's question. You can use the following entities: ['Gene', 'Disease'], relationships: ['PERTURBED'], and properties: {'Disease': ['name', 'ICD10', 'DSM5']}. Given the following valid combinations of source, relationship, and target: '(:Disease)-(:PERTURBED)->(:Protein)', '(:Disease)-(:PERTURBED)->(:Gene)', generate a Cypher query using one of these combinations. Only return the query, without any additional text.")
-        score = []
-        score.append("MATCH" in query)
-        score.append("RETURN" in query)
-        score.append("Gene" in query)
-        score.append("Disease" in query)
-        score.append("mucoviscidosis" in query)
-        score.append(
-            (
-                "-[:PERTURBED]->(g:Gene)" in query
-                or "(g:Gene)<-[:PERTURBED]-" in query
-            )
-        )
-        score.append("WHERE" in query or "{name:" in query)
-    
-        with open(FILE_PATH, "a") as f:
-            f.write(
-                f"{prompt_engine.model_name},cypher query,{calculate_test_score(score)}\n"
-            )
+    query = prompt_engine._generate_query(
+        question="Which genes are associated with mucoviscidosis?",
+        entities=["Gene", "Disease"],
+        relationships={
+            "PERTURBED": {"source": "Disease", "target": ["Protein", "Gene"]}
+        },
+        properties={"Disease": ["name", "ICD10", "DSM5"]},
+        query_language="Cypher",
+    )
 
-@pytest.mark.skip(reason="temporarily skip")
+    score = []
+    score.append("MATCH" in query)
+    score.append("RETURN" in query)
+    score.append("Gene" in query)
+    score.append("Disease" in query)
+    score.append("mucoviscidosis" in query)
+    score.append(
+        (
+            "-[:PERTURBED]->(g:Gene)" in query
+            or "(g:Gene)<-[:PERTURBED]-" in query
+        )
+    )
+    score.append("WHERE" in query or "{name:" in query)
+
+    with open(FILE_PATH, "a") as f:
+        f.write(
+            f"{prompt_engine.model_name},cypher query,{calculate_test_score(score)}\n"
+        )
+
+
 def test_end_to_end_query_generation(prompt_engine):
     query = prompt_engine.generate_query(
         question="Which genes are associated with mucoviscidosis?",
