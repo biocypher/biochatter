de588c7097f8885bf463bd26fc0499a4eefe5b89
Sebastian Lobentanzer <sebastian.lobentanzer@gmail.com>
2023-12-14 19:07:03 +0100

    formatting, comments

--- test/test_query_interaction.py
+++ test/test_query_interaction.py
@@ -6,12 +6,112 @@ import pytest
 from biochatter.query_interaction import BioCypherQueryHandler
 
 
-_kg = {'entities': {'Protein': {'represented_as': 'node', 'is_relationship': False, 'present_in_knowledge_graph': True, 'preferred_id': 'uniprot', 'input_label': 'protein', 'db_collection_name': 'proteins', 'properties': {'name': 'str', 'score': 'float', 'taxon': 'int', 'genes': 'str[]'}}, 'Gene': {'represented_as': 'node', 'is_relationship': False, 'present_in_knowledge_graph': True, 'preferred_id': 'hgnc', 'input_label': ['hgnc', 'ensg'], 'exclude_properties': 'accession'}, 'Disease': {'represented_as': 'node', 'is_relationship': False, 'present_in_knowledge_graph': True, 'preferred_id': 'doid', 'input_label': 'Disease', 'properties': {'name': 'str', 'ICD10': 'str', 'DSM5': 'str'}}}, 'properties': {'Disease': {'name': {}, 'ICD10': {}, 'DSM5': {}}, 'Protein': {'name': {}, 'score': {}, 'taxon': {}, 'genes': {}}, 'GeneToPhenotypeAssociation': {'score': {}, 'source': {}, 'evidence': {}}}, 'relationships': {'Phosphorylation': {'is_a': 'post translational interaction', 'is_relationship': True, 'present_in_knowledge_graph': True, 'represented_as': 'edge', 'input_label': 'phosphorylation', 'source': 'Protein', 'target': 'Protein'}, 'GeneToPhenotypeAssociation': {'represented_as': 'edge', 'is_relationship': True, 'present_in_knowledge_graph': True, 'label_as_edge': 'PERTURBED', 'input_label': ['protein_disease', 'gene_disease'], 'source': 'Disease', 'target': ['Protein', 'Gene'], 'exclude_properties': 'accession', 'properties': {'score': 'float', 'source': 'str', 'evidence': 'str'}}, 'GeneToDiseaseAssociation': {'represented_as': 'edge', 'is_relationship': True, 'present_in_knowledge_graph': True, 'label_as_edge': 'PERTURBED_IN', 'input_label': 'gene_phenotype', 'source': 'Protein', 'target': 'Disease', 'properties': {'score': 'float', 'source': 'str', 'evidence': 'str'}}}}
-_kg_selected = {'entities': ['Gene', 'Disease', 'Protein'], 'properties': {'Disease': {'name': {}, 'ICD10': {}, 'DSM5': {}}, 'Protein': {'name': {}, 'score': {}, 'taxon': {}, 'genes': {}}, 'GeneToPhenotypeAssociation': {'score': {}, 'source': {}, 'evidence': {}}}, 'relationships': {'PERTURBED': {'source': 'Disease', 'target': ['Protein', 'Gene']}}}
+_kg = {
+    "entities": {
+        "Protein": {
+            "represented_as": "node",
+            "is_relationship": False,
+            "present_in_knowledge_graph": True,
+            "preferred_id": "uniprot",
+            "input_label": "protein",
+            "db_collection_name": "proteins",
+            "properties": {
+                "name": "str",
+                "score": "float",
+                "taxon": "int",
+                "genes": "str[]",
+            },
+        },
+        "Gene": {
+            "represented_as": "node",
+            "is_relationship": False,
+            "present_in_knowledge_graph": True,
+            "preferred_id": "hgnc",
+            "input_label": ["hgnc", "ensg"],
+            "exclude_properties": "accession",
+        },
+        "Disease": {
+            "represented_as": "node",
+            "is_relationship": False,
+            "present_in_knowledge_graph": True,
+            "preferred_id": "doid",
+            "input_label": "Disease",
+            "properties": {"name": "str", "ICD10": "str", "DSM5": "str"},
+        },
+    },
+    "properties": {
+        "Disease": {"name": {}, "ICD10": {}, "DSM5": {}},
+        "Protein": {"name": {}, "score": {}, "taxon": {}, "genes": {}},
+        "GeneToPhenotypeAssociation": {
+            "score": {},
+            "source": {},
+            "evidence": {},
+        },
+    },
+    "relationships": {
+        "Phosphorylation": {
+            "is_a": "post translational interaction",
+            "is_relationship": True,
+            "present_in_knowledge_graph": True,
+            "represented_as": "edge",
+            "input_label": "phosphorylation",
+            "source": "Protein",
+            "target": "Protein",
+        },
+        "GeneToPhenotypeAssociation": {
+            "represented_as": "edge",
+            "is_relationship": True,
+            "present_in_knowledge_graph": True,
+            "label_as_edge": "PERTURBED",
+            "input_label": ["protein_disease", "gene_disease"],
+            "source": "Disease",
+            "target": ["Protein", "Gene"],
+            "exclude_properties": "accession",
+            "properties": {
+                "score": "float",
+                "source": "str",
+                "evidence": "str",
+            },
+        },
+        "GeneToDiseaseAssociation": {
+            "represented_as": "edge",
+            "is_relationship": True,
+            "present_in_knowledge_graph": True,
+            "label_as_edge": "PERTURBED_IN",
+            "input_label": "gene_phenotype",
+            "source": "Protein",
+            "target": "Disease",
+            "properties": {
+                "score": "float",
+                "source": "str",
+                "evidence": "str",
+            },
+        },
+    },
+}
+_kg_selected = {
+    "entities": ["Gene", "Disease", "Protein"],
+    "properties": {
+        "Disease": {"name": {}, "ICD10": {}, "DSM5": {}},
+        "Protein": {"name": {}, "score": {}, "taxon": {}, "genes": {}},
+        "GeneToPhenotypeAssociation": {
+            "score": {},
+            "source": {},
+            "evidence": {},
+        },
+    },
+    "relationships": {
+        "PERTURBED": {"source": "Disease", "target": ["Protein", "Gene"]}
+    },
+}
+
 
 @pytest.fixture
 def query_handler_basic():
-    query = "MATCH (d:Disease {name: 'mucoviscidosis'})-[:PERTURBED]->(g:Gene)\nRETURN g.name AS AssociatedGenes"
+    query = (
+        "MATCH (d:Disease {name: 'mucoviscidosis'})-[:PERTURBED]->(g:Gene) "
+        "RETURN g.name AS AssociatedGenes"
+    )
     query_lang = "Cypher"
     question = "Which genes are associated with mucoviscidosis?"
     return BioCypherQueryHandler(query, query_lang, _kg_selected, question)
@@ -19,7 +119,10 @@ def query_handler_basic():
 
 @pytest.fixture
 def query_handler_with_kg():
-    query = "MATCH (d:Disease {name: 'mucoviscidosis'})-[:PERTURBED]->(g:Gene)\nRETURN g.name AS AssociatedGenes"
+    query = (
+        "MATCH (d:Disease {name: 'mucoviscidosis'})-[:PERTURBED]->(g:Gene) "
+        "RETURN g.name AS AssociatedGenes"
+    )
     query_lang = "Cypher"
     question = "Which genes are associated with mucoviscidosis?"
     return BioCypherQueryHandler(query, query_lang, _kg_selected, question, _kg)
@@ -28,7 +131,7 @@ def query_handler_with_kg():
 @pytest.fixture
 def query_with_limit_handler():
     query = (
-        "MATCH (d:Disease {name: 'mucoviscidosis'})-[:PERTURBED]->(g:Gene)\n"
+        "MATCH (d:Disease {name: 'mucoviscidosis'})-[:PERTURBED]->(g:Gene) "
         "RETURN g.name AS AssociatedGenes LIMIT 25"
     )
     query_lang = "Cypher"
@@ -50,27 +153,32 @@ def test_explain(query_handler, request):
         explanation = query_handler.explain_query()
 
         mock_append_system_messages.assert_called_once_with(
-            f"You are an expert in {query_handler.query_lang} and will assist in explaining a query.\n"
-            f"The query answers the following user question: '{query_handler.question}'."
-            "It will be used to query a knowledge graph that contains (among others)"
-            f" the following entities: {query_handler.kg_selected['entities']}, "
-            f"relationships: {list(query_handler.kg_selected['relationships'].keys())}, and "
+            f"You are an expert in {query_handler.query_lang} and will assist "
+            "in explaining a query.\n"
+            "The query answers the following user question: "
+            f"'{query_handler.question}'."
+            "It will be used to query a knowledge graph that contains (among "
+            "others) the following entities: "
+            f"{query_handler.kg_selected['entities']}, "
+            "relationships: "
+            f"{list(query_handler.kg_selected['relationships'].keys())}, and "
             f"properties: {query_handler.kg_selected['properties']}. "
             "Only return the explanation, without any additional text."
         )
-    # TODO what other checks can we do? The explanation text is not predictable
-    #  (except if we would add desired formatting in the future)
 
 
 def system_msg_for_update(query_handler):
     return (
-        f"You are an expert in {query_handler.query_lang} and will assist in updating a query.\n"
-        f"The original query answers the following user question: '{query_handler.question}'."
+        f"You are an expert in {query_handler.query_lang} and will assist in "
+        "updating a query.\n"
+        "The original query answers the following user question: "
+        f"'{query_handler.question}'."
         f"This is the original query: '{query_handler.query}'."
-        f"It will be used to query a knowledge graph that has the following entities: "
-        f"{query_handler.kg['entities']}, relationships: {list(query_handler.kg['relationships'].keys())}, and "
+        f"It will be used to query a knowledge graph that has the following "
+        f"entities: {query_handler.kg['entities']}, relationships: "
+        f"{list(query_handler.kg['relationships'].keys())}, and "
         f"properties: {query_handler.kg['properties']}. "
-        "Update the query to reflect the user's request."
+        "Update the query to reflect the user's request. "
         "Only return the updated query, without any additional text."
     )
 
@@ -94,9 +202,10 @@ def test_update_limit(query_with_limit_handler):
 
 def update_limit(query_handler, update_request):
     with patch("biochatter.query_interaction.GptConversation") as mock_gptconv:
-        resultMsg = """
-        MATCH (d:Disease {name: 'mucoviscidosis'})-[:PERTURBED]->(g:Gene)\nRETURN g.name AS AssociatedGenes LIMIT 10
-        """
+        resultMsg = (
+            "MATCH (d:Disease {name: 'mucoviscidosis'})-[:PERTURBED]->(g:Gene) "
+            "RETURN g.name AS AssociatedGenes LIMIT 10"
+        )
         mock_gptconv.return_value.query.return_value = [resultMsg, Mock(), None]
         mock_append_system_messages = Mock()
         mock_gptconv.return_value.append_system_message = (
@@ -115,9 +224,10 @@ def test_update_sorting(query_handler, request):
     query_handler = request.getfixturevalue(query_handler)
     update_request = "Sort the results alphabetically"
     with patch("biochatter.query_interaction.GptConversation") as mock_gptconv:
-        resultMsg = """
-        MATCH (d:Disease {name: \'mucoviscidosis\'})-[:PERTURBED]->(g:Gene)\nRETURN g.name AS AssociatedGenes\nORDER BY g.name ASC
-        """
+        resultMsg = (
+            "MATCH (d:Disease {name: 'mucoviscidosis'})-[:PERTURBED]->(g:Gene) "
+            "RETURN g.name AS AssociatedGenes\nORDER BY g.name ASC"
+        )
         mock_gptconv.return_value.query.return_value = [resultMsg, Mock(), None]
         mock_append_system_messages = Mock()
         mock_gptconv.return_value.append_system_message = (
@@ -135,6 +245,7 @@ def test_update_sorting(query_handler, request):
 # other tests that can be implemented:
 def test_update_filtering():
     assert True
+
 
 def test_update_returned_properties():
     assert True
