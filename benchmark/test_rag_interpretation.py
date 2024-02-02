adb5b0e42fe610b95c065253bcad886bc814c8d3
slobentanzer <sebastian.lobentanzer@gmail.com>
2024-02-02 13:51:15 +0100

    RAG benchmark: remove getting test data .. via function, just access the keys of the dictionary

--- benchmark/test_rag_interpretation.py
+++ benchmark/test_rag_interpretation.py
@@ -11,65 +11,33 @@ from .benchmark_utils import (
 )
 
 
-def get_test_data(test_data_rag_interpretation: list) -> tuple:
-    """
-
-    Helper function to unpack the test data from the
-    test_data_rag_interpretation fixture.
-
-    Args:
-        test_data_rag_interpretation (list): The test data from the
-        test_data_rag_interpretation fixture
-
-    Returns:
-        tuple: The unpacked test data
-    """
-    return (
-        test_data_rag_interpretation["system_messages"],
-        test_data_rag_interpretation["prompt"],
-        test_data_rag_interpretation["entities"],
-        test_data_rag_interpretation["test_case_purpose"],
-        test_data_rag_interpretation["index"],
-    )
-
-
 def test_explicit_relevance_of_single_fragments(
     model_name,
     test_data_rag_interpretation,
     conversation,
     multiple_testing,
 ):
-    (
-        system_messages,
-        prompt,
-        expected_answers,
-        test_case_purpose,
-        test_case_index,
-    ) = get_test_data(test_data_rag_interpretation)
     task = f"{inspect.currentframe().f_code.co_name.replace('test_', '')}"
-    subtask = f"{str(test_case_index)}_{test_case_purpose}"
-    if not test_case_purpose == "explicit":
+    subtask = f"{str(test_data_rag_interpretation['hash'])}_{test_data_rag_interpretation['test_case_purpose']}"
+    if not test_data_rag_interpretation["test_case_purpose"] == "explicit":
         pytest.skip(
-            f"test case {test_case_purpose} not supported for {subtask} benchmark"
+            f"test case {test_data_rag_interpretation['test_case_purpose']} not supported for {subtask} benchmark"
         )
     skip_if_already_run(model_name=model_name, task=task, subtask=subtask)
 
     def run_test():
         conversation.reset()  # needs to be reset for each test
-        [conversation.append_system_message(m) for m in system_messages]
-        response, _, _ = conversation.query(prompt)
-        answers = ensure_iterable(response.split(","))
+        [
+            conversation.append_system_message(m)
+            for m in test_data_rag_interpretation["system_messages"]
+        ]
+        response, _, _ = conversation.query(
+            test_data_rag_interpretation["prompt"]
+        )
 
         score = []
 
-        if len(answers) == len(expected_answers):
-            for index, answer in enumerate(answers):
-                if answer == expected_answers[index]:
-                    score.append(True)
-                else:
-                    score.append(False)
-        else:
-            [score.append(False) for _ in expected_answers]
+        score.append(response.lower() == test_data_rag_interpretation["answer"])
 
         return calculate_test_score(score)
 
@@ -91,25 +59,23 @@ def test_implicit_relevance_of_multiple_fragments(
     evaluation_conversation,
     multiple_testing,
 ):
-    (
-        system_messages,
-        prompt,
-        expected_answers,
-        test_case_purpose,
-        test_case_index,
-    ) = get_test_data(test_data_rag_interpretation)
     task = f"{inspect.currentframe().f_code.co_name.replace('test_', '')}"
-    subtask = f"{str(test_case_index)}_{test_case_purpose}"
-    if not test_case_purpose == "implicit":
+    subtask = f"{str(test_data_rag_interpretation['hash'])}_{test_data_rag_interpretation['test_case_purpose']}"
+    if not test_data_rag_interpretation["test_case_purpose"] == "implicit":
         pytest.skip(
-            f"test case {test_case_purpose} not supported for {subtask} benchmark"
+            f"test case {test_data_rag_interpretation['test_case_purpose']} not supported for {subtask} benchmark"
         )
     skip_if_already_run(model_name=model_name, task=task, subtask=subtask)
 
     def run_test():
         conversation.reset()  # needs to be reset for each test
-        [conversation.append_system_message(m) for m in system_messages]
-        response, _, _ = conversation.query(prompt)
+        [
+            conversation.append_system_message(m)
+            for m in test_data_rag_interpretation["system_messages"]
+        ]
+        response, _, _ = conversation.query(
+            test_data_rag_interpretation["prompt"]
+        )
 
         # evaluator LLM
         evaluation_conversation.append_system_message(
