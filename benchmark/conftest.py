d2f43fcbc834c4d6b96e32dbdb9ef18118c0af07
slobentanzer <sebastian.lobentanzer@gmail.com>
2024-02-07 14:50:32 +0100

    n iter to 1 for testing

--- benchmark/conftest.py
+++ benchmark/conftest.py
@@ -12,7 +12,7 @@ from biochatter.llm_connect import GptConversation, XinferenceConversation
 from .benchmark_utils import benchmark_already_executed
 
 # how often should each benchmark be run?
-N_ITERATIONS = 2
+N_ITERATIONS = 1
 
 # which dataset should be used for benchmarking?
 BENCHMARK_DATASET = get_benchmark_dataset()
