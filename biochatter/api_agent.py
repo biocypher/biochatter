b6a70f2dd842eb4ed84c31c1ed5b20f3d7ce1d35
Tatan47 <noah.bruderer@gmail.com>
2024-05-16 16:07:01 +0200

    Updated the api_agent, it is now functional and can extract the users question from the fetched BLAST file

--- biochatter/api_agent.py
+++ biochatter/api_agent.py
@@ -15,11 +15,6 @@
 ###
 ###     TO DO: Handle all dependencies and imports with poetry 
 ###
-from langchain.prompts import ChatPromptTemplate
-from langchain.chat_models import ChatOpenAI
-from langchain.chains.openai_functions import (
-    create_structured_output_runnable,
-)
 
 from pydantic import BaseModel, Field
 from typing import Optional
@@ -29,62 +24,35 @@ import requests
 import os 
 import re
 import time
+from typing import Optional
 
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.pydantic_v1 import BaseModel, Field
+from langchain_core.output_parsers import StrOutputParser
+from langchain.prompts import ChatPromptTemplate
+from langchain.chat_models import ChatOpenAI
+
+from langchain.chains.openai_functions import (
+    create_structured_output_runnable,
+)
 
 ###
 ###     TO DO: IMPLEMENT LLM CALLING LOGIC THROUGH llm_connect.py
 ###
-llm = ChatOpenAI()
-
+openai_api_key = os.getenv("OPENAI_API_KEY")
+llm = ChatOpenAI(model_name='gpt-4', temperature=0, openai_api_key=openai_api_key)
+    
+    
 class BlastQueryRequest(BaseModel):
     """BlastQueryRequest is a Pydantic model for the BLAST query request.
     A Pydantic model for configuring and sending a request to the NCBI BLAST query API.
-
-    Args:
-        url (str): The base URL for the BLAST API.
-        
-        cmd (str): The command type, either 'Put' to submit a new query 
-            or 'Get' to retrieve results from a previous query.
-        
-        program (Optional[str]): The specific BLAST program to use, 'blastn' for nucleotide BLASTn. 
-            'blastp' for BLASTp.
-        
-        database (str): The database against which the query will be run.
-            e.g., 'nt' for nucleotide database. Defaults to 'nt'.
-        
-        query (Optional[str]): The nucleotide or protein sequence to be queried. 
-        
-        format_type (Optional[str]): The format for the BLAST results, choices include 'Text', 'XML', etc. 
-            Defaults to 'Text'.
-        
-        rid (Optional[str]): A Request ID used to fetch results from a specific query.
-        
-        other_params (Optional[dict]): Additional parameters for the BLAST query, 
-            such as user email. Defaults to {'email': 'noah.bruderer@uib.no'}. MUST BE CHANGED
-        
-        max_hits (int): The maximum number of query results to return. 
-            Defaults to 15.
-        
-        sort_by (Optional[str]): The criterion for sorting the results.
-            e.g., 'score' or 'evalue'. Defaults to 'score'.
-        
-        megablast (str): A flag set to 'on' specifically for alignments against the human genome. 
-            Defaults to 'on'.
-        
-        question_uuid (Optional[str]): Automatically generated unique identifier for each query. 
-        
-        full_url (Optional[str]): The full URL used for the query. 
-            This is to be formatted before running the query. Defaults to 'TBF' (To Be Formatted).
-
-    Usage:
-        This class is used to create a structured output to construct a BLAST query that is then sent via a network request to the NCBI servers.
     """
     
-    url: str = Field(
+    url: Optional[str] = Field(
         default="https://blast.ncbi.nlm.nih.gov/Blast.cgi?",
         description="ALWAYS USE DEFAULT, DO NOT CHANGE"
     )
-    cmd: str = Field(
+    cmd: Optional[str] = Field(
         default="Put",
         description="Command to execute, 'Put' for submitting query, 'Get' for retrieving results."
     )
@@ -92,7 +60,7 @@ class BlastQueryRequest(BaseModel):
         default="blastn",
         description="BLAST program to use, e.g., 'blastn' for nucleotide-nucleotide BLAST, 'blastp' for protein-protein BLAST."
     )
-    database: str = Field(
+    database: Optional[str] = Field(
         default="nt",
         description="Database to search, e.g., 'nt' for nucleotide database, 'nr' for non redundant protein database, pdb the Protein Data Bank database, which is used specifically for protein structures, 'refseq_rna' and 'refseq_genomic': specialized databases for RNA sequences and genomic sequences"
     )
@@ -112,7 +80,7 @@ class BlastQueryRequest(BaseModel):
         default={"email": "noah.bruderer@uib.no"},
         description="Other optional BLAST parameters, including user email."
     )
-    max_hits: int = Field(
+    max_hits: Optional[int] = Field(
         default=15,
         description="Maximum number of hits to return in the BLAST results."
     )
@@ -120,7 +88,7 @@ class BlastQueryRequest(BaseModel):
         default="score",
         description="Criterion to sort BLAST results by, e.g., 'score', 'evalue'."
     )
-    megablast: str = Field(
+    megablast: Optional[str] = Field(
         default="on", 
         description="Set to 'on' for human genome alignemnts"
     )
@@ -132,18 +100,9 @@ class BlastQueryRequest(BaseModel):
         default='TBF',
         description="Url used for the blast query"
     )
-    
+
+
 def BLAST_api_query_generator(question: str, BLAST_prompt_file_path: str) -> BlastQueryRequest:
-    """Function to write api call for any BLAST query
-    
-    Args:
-        question (str): The question to be answered using BLAST.
-        
-        BLAST_prompt_file_path (str): The path to the BLAST prompt file.
-        
-    Returns: 
-        BlastQueryRequest: Pydantic model for configuring and sending a request to the NCBI BLAST query API.
-    """
     BLAST_structured_output_prompt = ChatPromptTemplate.from_messages(
         [
             (
@@ -157,7 +116,6 @@ def BLAST_api_query_generator(question: str, BLAST_prompt_file_path: str) -> Bla
             ("human", "Tip: Make sure to answer in the correct format"),
         ]
     )
-    #here we read the BLAST prompt from the file
     try:
         with open(BLAST_prompt_file_path, 'r') as file:
             BLAST_prompt = file.read()
@@ -165,12 +123,19 @@ def BLAST_api_query_generator(question: str, BLAST_prompt_file_path: str) -> Bla
         print("The file was not found at the specified path.")
     except Exception as e:
         print("An error occurred while reading the file:", str(e))
-    #now we create a runnable object that will use the specific input to fill in all fields in the BlastQueryRequest
+
+    # Debugging: print the prompt to ensure it is read correctly
+    print(f"BLAST Prompt: {BLAST_prompt}")
+        
     runnable = create_structured_output_runnable(BlastQueryRequest, llm, BLAST_structured_output_prompt)
     blast_call_obj = runnable.invoke({"input": f"Answer:\n{question} based on:\n {BLAST_prompt}"})
-    #create a uuid for the specific question: this way we can track the question and all downstream processes that are triggered by it
-    blast_call_obj.question_uuid=str(uuid.uuid4())
+    
+    # Debugging: print the generated blast_call_obj to check its structure
+    print(f"Generated BLAST Call Object: {blast_call_obj}")
+    
+    blast_call_obj.question_uuid = str(uuid.uuid4())
     return blast_call_obj
+
 
 def submit_blast_query(request_data: BlastQueryRequest) -> str:
     """Function to POST the BLAST query and retrieve RID.
@@ -208,14 +173,6 @@ def submit_blast_query(request_data: BlastQueryRequest) -> str:
         return match.group(1)
     else:
         raise ValueError("RID not found in BLAST submission response.")
-
-
-
-
-
-
-
-
 
 def fetch_and_save_blast_results(request_data: BlastQueryRequest, blast_query_return: str, save_path: str , 
                                 max_attempts: int = 10000):
@@ -279,21 +236,93 @@ def fetch_and_save_blast_results(request_data: BlastQueryRequest, blast_query_re
     return file_name
 
 
-def run_me():
+    
+# blast_prompt_path = "docs/api_agent/BLAST_tool/persistent_files/api_documentation/BLAST.txt"
+# base_dir = os.getcwd()
+
+def api_agent(question, blast_prompt_path, BLAST_result_dir):
     """Short term solution to test"""
-    blast_prompt_path = "docs/api_agent/BLAST_tool/persistent_files/api_documentation/BLAST.txt"
     base_dir = os.getcwd()
     BLAST_prompt_file_path = os.path.join(base_dir, blast_prompt_path)
-    #blat_result 
-    BLAST_result_dir = "docs/api_agent/BLAST_tool/BLAST_response"
+    #blast_result 
     BLAST_result_path = os.path.join(base_dir, BLAST_result_dir)
-    question = "Which organism does the DNA sequence come from:TTCATCGGTCTGAGCAGAGGATGAAGTTGCAAATGATGCAAGCAAAACAGCTCAAAGATGAAGAGGAAAAGGCTATACACAACAGGAGCAATGTAGATACAGAAGGT"
     query_request = BLAST_api_query_generator(question, BLAST_prompt_file_path)
     print(query_request)
     rid = submit_blast_query(query_request)
     print(rid)
-    BLAST_file_name = fetch_and_save_blast_results(query_request, rid, BLAST_result_path, question)
+    BLAST_file_name = fetch_and_save_blast_results(query_request, rid, BLAST_result_path, 10000)
     print(BLAST_file_name)
-    
+    return BLAST_file_name
 
-run_me()
+
+### answer extraction 
+
+from langchain_core.prompts import ChatPromptTemplate
+
+# file_path = 'docs/api_agent/BLAST_tool/BLAST_response/BLAST_results_1a4862a5-b185-4c87-a24b-a83c1d1bc2b4.txt'
+
+def read_first_n_lines(file_path: str, n: int):
+    """
+    Reads the first n lines from a file and returns them as a string.
+
+    Args:
+        file_path (str): The path to the file.
+        n (int): The number of lines to read.
+
+    Returns:
+        str: The first n lines from the file as a string.
+
+    Raises:
+        FileNotFoundError: If the file is not found.
+        Exception: If any other error occurs during file reading.
+
+    """
+    try:
+        with open(file_path, 'r') as file:
+            lines = []
+            for i in range(n):
+                line = file.readline()
+                if not line:
+                    break
+                lines.append(line.strip())
+            #to test:
+            #more efficient with \n or without?
+            return '\n'.join(lines)
+    except FileNotFoundError:
+        return "The file was not found."
+    except Exception as e:
+        return f"An error occurred: {e}"
+
+
+def answer_extraction(question:str, file_path: str, n: int) -> str:
+    """
+    Function to extract the answer from the BLAST results.
+
+    Args:
+        question (str): The question to be answered.
+        file_path (str): The path to the BLAST results file.
+        n (int): The number of lines to read from the file.
+
+    Returns:
+        str: The extracted answer from the BLAST results.
+
+    """
+    prompt = ChatPromptTemplate.from_messages([
+    ("system", "You are a world class molecular biologist who knows everything about NCBI and BLAST results."),
+    ("user", "{input}")
+    ])
+
+    context = read_first_n_lines(file_path, n)
+    BLAST_file_answer_extractor_prompt = f"""
+            You have to answer the question: {question} in a clear and concise manner. Be factual!\n\
+            If you are asked what organism a specific sequence belongs to, check the 'Hit_def' fields. If you find a synthetic construct or predicted entry, move to the next one and look for an organism name.\n\
+            Try to use the hits with the best identity score to answer the question. If it is not possible, move to the next one.\n\
+            Be clear, and if organism names are present in ANY of the results, please include them in the answer. Do not make up information and mention how relevant the found information is based on the identity scores.\n\
+            Use the same reasoning for any potential BLAST results. If you find information that is manually curated, please use it and state it. You may also state other results, but always include the context.\n\
+            Based on the information given here:\n\
+            {context}
+            """
+    output_parser = StrOutputParser()
+    chain = prompt | llm | output_parser
+    BLAST_answer = chain.invoke({"input": {BLAST_file_answer_extractor_prompt}})
+    return BLAST_answer
