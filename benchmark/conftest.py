a1f4807f8745f4dc46d99ebd0df32efdde612be0
slobentanzer <sebastian.lobentanzer@gmail.com>
2024-06-19 16:43:46 +0200

    another batch of models run

--- benchmark/conftest.py
+++ benchmark/conftest.py
@@ -25,31 +25,32 @@ OPENAI_MODEL_NAMES = [
     # "gpt-3.5-turbo-0125",
     # "gpt-4-0613",
     # "gpt-4-0125-preview",
+    # "gpt-4o-2024-05-13",
 ]
 
 XINFERENCE_MODELS = {
-    "llama-2-chat": {
-        "model_size_in_billions": [
-            7,
-            13,
-            # 70,
-        ],
-        "model_format": "ggufv2",
-        "quantization": [
-            # "Q2_K",
-            # "Q3_K_S",
-            # "Q3_K_M",
-            # "Q3_K_L",
-            # "Q4_0",
-            # "Q4_K_S",
-            "Q4_K_M",
-            # "Q5_0",
-            # "Q5_K_S",
-            # "Q5_K_M",
-            # "Q6_K",
-            # "Q8_0",
-        ],
-    },
+    # "llama-2-chat": {
+    #     "model_size_in_billions": [
+    #         7,
+    #         13,
+    #         # 70,
+    #     ],
+    #     "model_format": "ggufv2",
+    #     "quantization": [
+    #         "Q2_K",
+    #         # "Q3_K_S",
+    #         "Q3_K_M",
+    #         # "Q3_K_L",
+    #         # "Q4_0",
+    #         # "Q4_K_S",
+    #         "Q4_K_M",
+    #         # "Q5_0",
+    #         # "Q5_K_S",
+    #         "Q5_K_M",
+    #         "Q6_K",
+    #         "Q8_0",
+    #     ],
+    # },
     # "code-llama-instruct": {
     #     "model_size_in_billions": [
     #         7,
@@ -78,14 +79,14 @@ XINFERENCE_MODELS = {
         ],
         "model_format": "ggufv2",
         "quantization": [
-            # "Q2_K",
+            "Q2_K",
             # "Q3_K_M",
             # "Q4_0",
             "Q4_K_M",
             # "Q5_0",
-            # "Q5_K_M",
-            # "Q6_K",
-            # "Q8_0",
+            "Q5_K_M",
+            "Q6_K",
+            "Q8_0",
         ],
     },
     "openhermes-2.5": {
@@ -94,18 +95,18 @@ XINFERENCE_MODELS = {
         ],
         "model_format": "ggufv2",
         "quantization": [
-            # "Q2_K",
+            "Q2_K",
             # "Q3_K_S",
-            # "Q3_K_M",
+            "Q3_K_M",
             # "Q3_K_L",
             # "Q4_0",
             # "Q4_K_S",
             "Q4_K_M",
             # "Q5_0",
             # "Q5_K_S",
-            # "Q5_K_M",
-            # "Q6_K",
-            # "Q8_0",
+            "Q5_K_M",
+            "Q6_K",
+            "Q8_0",
         ],
     },
     "chatglm3": {
@@ -123,18 +124,18 @@ XINFERENCE_MODELS = {
         ],
         "model_format": "ggufv2",
         "quantization": [
-            # "Q2_K",
+            "Q2_K",
             # "Q3_K_S",
-            # "Q3_K_M",
+            "Q3_K_M",
             # "Q3_K_L",
             # "Q4_0",
             # "Q4_K_S",
             "Q4_K_M",
             # "Q5_0",
             # "Q5_K_S",
-            # "Q5_K_M",
-            # "Q6_K",
-            # "Q8_0",
+            "Q5_K_M",
+            "Q6_K",
+            "Q8_0",
         ],
     },
     # "gemma-it": {
@@ -157,11 +158,11 @@ XINFERENCE_MODELS = {
         "model_format": "ggufv2",
         "quantization": [
             # 8B model quantisations
-            # "IQ3_M",
+            "IQ3_M",
             "Q4_K_M",
-            # "Q5_K_M",
-            # "Q6_K",
-            # "Q8_0",
+            "Q5_K_M",
+            "Q6_K",
+            "Q8_0",
             # 70B model quantisations
             # "IQ1_M",
             # "IQ2_XS",
@@ -317,6 +318,9 @@ def conversation(request, model_name, client):
             model_name=model_name,
             prompts={},
             correct=False,
+        )
+        conversation.set_api_key(
+            os.getenv("OPENAI_API_KEY"), user="benchmark_user"
         )
     elif model_name in XINFERENCE_MODEL_NAMES:
         (
