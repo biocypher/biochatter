ca147138a8edc5f70bef9aa128585af603154b73
fengsh <shaohong.feng.78@gmail.com>
2024-07-26 21:22:39 -0400

    remove test code

--- test/test_decider_agent.py
+++ test/test_decider_agent.py
@@ -1,14 +1,12 @@
 
 import json
 from typing import Dict, List
-from langchain_openai import AzureOpenAIEmbeddings
 import pytest
 from unittest.mock import patch, MagicMock
 import os
 from dotenv import load_dotenv
 import neo4j_utils as nu
 import logging
-from langchain.schema import Document
 from langchain_core.messages import (
     BaseMessage,
     AIMessage,
@@ -19,7 +17,7 @@ from langchain.output_parsers.openai_tools import (
 import shortuuid
 
 from biochatter.decider_agent import ChooseRagAgent, DeciderAgent, ReviseRagAgent
-from biochatter.langgraph_agent_base import ReflexionAgentResult, ResponderWithRetries
+from biochatter.langgraph_agent_base import ResponderWithRetries
 from biochatter.llm_connect import AzureGptConversation
 from biochatter.rag_agent import RagAgent, RagAgentModeEnum
 
@@ -61,99 +59,6 @@ def create_conversation():
     )
     chatter.set_api_key(os.environ["OPENAI_API_KEY"])
     return chatter
-
-
-@pytest.fixture
-def vectorAgent():
-    return RagAgent(
-        mode=RagAgentModeEnum.VectorStore,
-        connection_args={
-            "host": "10.95.224.94",
-            "port": "19530",
-        },
-        conversation_factory=create_conversation,
-        embedding_func=AzureOpenAIEmbeddings(
-            api_key=os.environ["OPENAI_API_KEY"],
-            azure_endpoint=os.environ["AZURE_OPENAI_ENDPOINT"],
-            azure_deployment=os.environ["AZURE_OPENAI_EMBEDDINGS_DEPLOYMENT_NAME"],
-            model=os.environ["AZURE_OPENAI_EMBEDDINGS_MODEL"]
-        ),
-        model_name="gpt-3.5-turbo",
-        use_prompt=True,
-    )
-
-@pytest.fixture
-def apiBlastAgent():
-    return RagAgent(
-        mode=RagAgentModeEnum.API_BLAST,
-        conversation_factory=create_conversation,
-        use_prompt=True,
-    )
-@pytest.fixture
-def apiOncoKBAgent():
-    return RagAgent(
-        mode=RagAgentModeEnum.API_ONCOKB,
-        conversation_factory=create_conversation,
-        use_prompt=True,
-    )
-
-@pytest.fixture
-def databaseAgent():
-    connection_args={
-        "host": "10.95.224.94",
-        "port": "47687",
-    }
-    schema_dict = find_schema_info_node(connection_args)
-    return RagAgent(
-        mode=RagAgentModeEnum.KG,
-        connection_args=connection_args,
-        schema_config_or_info_dict=schema_dict,
-        model_name="gpt-3.5-turbo",
-        conversation_factory=create_conversation,
-        use_prompt=True,
-    )
-@pytest.mark.skip()
-def test_decider_agent_kg(vectorAgent, databaseAgent):
-    decider_agent = DeciderAgent(
-        rag_agents=[vectorAgent, databaseAgent],
-        conversation_factory=create_conversation
-    )
-    result = decider_agent.execute("what are the biological functions of the gene SETBP1")
-    assert result is not None
-    logs = decider_agent.get_logs()
-    with open("./temp-3.log", "a+") as fobj:
-        fobj.write(logs)
-
-@pytest.mark.skip()
-def test_decider_agent_vectorstore(vectorAgent, databaseAgent):
-    decider_agent = DeciderAgent(
-        rag_agents=[vectorAgent, databaseAgent],
-        conversation_factory=create_conversation
-    )
-    result = decider_agent.execute("How to reset trip for Lexus LS 460 after maintenance")
-    assert result is not None
-    logs = decider_agent.get_logs()
-    with open("./temp-3.log", "a+") as fobj:
-        fobj.write(logs)
-
-@pytest.mark.skip()
-def test_decider_agent_api(
-        vectorAgent, 
-        databaseAgent, 
-        apiBlastAgent, 
-        apiOncoKBAgent
-    ):
-    decider_agent = DeciderAgent(
-        rag_agents=[
-            vectorAgent, databaseAgent, apiBlastAgent, apiOncoKBAgent
-        ],
-        conversation_factory=create_conversation
-    )
-    result = decider_agent.execute("What is the oncogenic potential of BRAF V600E mutation?")
-    assert result is not None
-    logs = decider_agent.get_logs()
-    with open("./temp-3.log", "a+") as fobj:
-        fobj.write(logs)
 
 class ChatOpenAIMock:
     def __init__(self) -> None:
