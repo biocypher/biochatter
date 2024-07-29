a7832351165d4c574e6d4ec8dcf5d14c49594227
slobentanzer <sebastian.lobentanzer@gmail.com>
2024-07-29 23:46:05 +0200

    add gpt models

--- benchmark/conftest.py
+++ benchmark/conftest.py
@@ -23,7 +23,9 @@ OPENAI_MODEL_NAMES = [
     "gpt-3.5-turbo-0125",
     "gpt-4-0613",
     "gpt-4-0125-preview",
+    "gpt-4-turbo-2024-04-09",
     "gpt-4o-2024-05-13",
+    "gpt-4o-mini-2024-07-18",
 ]
 
 XINFERENCE_MODELS = {
