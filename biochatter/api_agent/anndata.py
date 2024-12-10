d56b82f8382d5b71ae41adc0ea8a83591f4142ea
Anis Ismail <anisismail09@gmail.com>
2024-12-10 13:25:06 +0100

    updated the prompt

--- biochatter/api_agent/anndata.py
+++ biochatter/api_agent/anndata.py
@@ -9,7 +9,7 @@
 
 ANNDATA_IO_QUERY_PROMPT = """
 You are a world class algorithm for creating queries in structured formats.
-Your task is to use the Python API of `anndata` to answer questions about the `anndata` io module.
-You should prefix the function calls with `anndata.io.`, for instance, for reading a file, you should use
-`anndata.io.read`.
+Your task is to use the Python API of `anndata` to answer questions about the `anndata` `io` module.
+You should prefix the function calls with `anndata.io.`, for instance, for reading a h5ad file, you should use
+`anndata.io.read_h5ad`.
 """
