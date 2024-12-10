c20a3c6bcc219ba1149d71852a23787bdb4ef4cd
noahbruderer <noah.bruderer@gmail.com>
2024-12-10 10:24:34 +0100

    Aim of the anndata api module

---
+++ biochatter/api_agent/anndata.py
@@ -0,0 +1,8 @@
+### Aim
+## For any question about anndata:
+## return the code to answer the question
+
+# 1. Read in anndata object from any anndata api supported format -> built-in anndata api
+# 2. Concatenate the anndata object -> built-in anndata api
+# 3. Filter the anndata object -> NumPy or SciPy sparse matrix api
+# 4. Write the anndata object to [xxx] format -> built-in anndata api
