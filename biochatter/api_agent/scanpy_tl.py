27e2abc610ff6a28f81a268558406e87d5e991a6
slobentanzer <sebastian.lobentanzer@gmail.com>
2024-12-12 15:51:03 +0100

    cleanup (prompt, docstrings, syntax)

--- biochatter/api_agent/scanpy_tl.py
+++ biochatter/api_agent/scanpy_tl.py
@@ -1,110 +1,69 @@
 """Module for interacting with the `scanpy` API for data tools (`tl`)."""
-from types import ModuleType
 
 from collections.abc import Callable
+from types import ModuleType
 from typing import TYPE_CHECKING
 
 from langchain_core.output_parsers import PydanticToolsParser
-from langchain_core.pydantic_v1 import BaseModel
 
 from .abc import BaseQueryBuilder, BaseAPIModel
-from biochatter.llm_connect import Conversation
 from .generate_pydantic_classes_from_module import generate_pydantic_classes
 
 if TYPE_CHECKING:
     from biochatter.llm_connect import Conversation
 
 SCANPY_QUERY_PROMPT = """
-You are a world class algorithm for creating queries in structured formats. Your task is to use the scanpy python package
-to provide the user with the appropriate function call to answer their question. You focus on the scanpy.tl module, which has 
-the following overview:
-Any transformation of the data matrix that is not *preprocessing*. In contrast to a *preprocessing* function, a *tool* usually adds an easily interpretable annotation to the data matrix, which can then be visualized with a corresponding plotting function.
+
+You are a world class algorithm for creating queries in structured formats. Your
+task is to use the scanpy python package to provide the user with the
+appropriate function call to answer their question. You focus on the scanpy.tl
+module, which has the following overview: Any transformation of the data matrix
+that is not *preprocessing*. In contrast to a *preprocessing* function, a *tool*
+usually adds an easily interpretable annotation to the data matrix, which can
+then be visualized with a corresponding plotting function.
 
 ### Embeddings
-
-```{eval-rst}
-.. autosummary::
-   :nosignatures:
-   :toctree: ../generated/
 
    pp.pca
    tl.tsne
    tl.umap
    tl.draw_graph
    tl.diffmap
-```
 
 Compute densities on embeddings.
 
-```{eval-rst}
-.. autosummary::
-   :nosignatures:
-   :toctree: ../generated/
-
    tl.embedding_density
-```
 
 ### Clustering and trajectory inference
-
-```{eval-rst}
-.. autosummary::
-   :nosignatures:
-   :toctree: ../generated/
 
    tl.leiden
    tl.louvain
    tl.dendrogram
    tl.dpt
    tl.paga
-```
 
 ### Data integration
 
-```{eval-rst}
-.. autosummary::
-   :nosignatures:
-   :toctree: ../generated/
-
    tl.ingest
-```
 
 ### Marker genes
-
-```{eval-rst}
-.. autosummary::
-   :nosignatures:
-   :toctree: ../generated/
 
    tl.rank_genes_groups
    tl.filter_rank_genes_groups
    tl.marker_gene_overlap
-```
 
 ### Gene scores, Cell cycle
 
-```{eval-rst}
-.. autosummary::
-   :nosignatures:
-   :toctree: ../generated/
-
    tl.score_genes
    tl.score_genes_cell_cycle
-```
 
 ### Simulations
 
-```{eval-rst}
-.. autosummary::
-   :nosignatures:
-   :toctree: ../generated/
-
    tl.sim
-
-```
 """
 
-class ScanpyTLQueryBuilder(BaseQueryBuilder):
-    """A class for building an ScanpyTLQuery object."""
+class ScanpyTlQueryBuilder(BaseQueryBuilder):
+    """A class for building an ScanpyTlQuery object."""
 
     def create_runnable(
         self,
@@ -124,10 +83,10 @@ class ScanpyTLQueryBuilder(BaseQueryBuilder):
     ):
         """Generate an ScanpyTLQuery object.
 
-        Generate a ScanpyTLQuery object based on the given question, prompt,
-        and BioChatter conversation. Uses a Pydantic model to define the API
-        fields. Using langchains .bind_tools method to allow the LLM to parameterise
-        the function call, based on the functions available in thescanpy.tl module.
+        Generate a ScanpyTLQuery object based on the given question, prompt, and
+        BioChatter conversation. Uses a Pydantic model to define the API fields.
+        Using langchains .bind_tools method to allow the LLM to parameterise the
+        function call, based on the functions available in thescanpy.tl module.
 
         Args:
         ----
@@ -144,13 +103,13 @@ class ScanpyTLQueryBuilder(BaseQueryBuilder):
         """
         if generated_classes is None:
             tools = generate_pydantic_classes(module)
-        
+
         runnable = self.create_runnable(
-            conversation=conversation, query_parameters=tools
+            conversation=conversation, query_parameters=tools,
         )
 
         query = [
-            ("system", SCANPY_QUERY_PROMPT),            
+            ("system", SCANPY_QUERY_PROMPT),
             ("human", question),
         ]
 
