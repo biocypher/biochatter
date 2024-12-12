e0a147a3a8dfc63c13209caf9fe9b358fe252351
slobentanzer <sebastian.lobentanzer@gmail.com>
2024-12-12 18:48:37 +0100

    fix imports, remove non-reduced builder

--- biochatter/api_agent/__init__.py
+++ biochatter/api_agent/__init__.py
@@ -17,6 +17,8 @@ from .blast import (
 from .formatters import format_as_python_call, format_as_rest_call
 from .oncokb import OncoKBFetcher, OncoKBInterpreter, OncoKBQueryBuilder
 from .scanpy_pl import ScanpyPlQueryBuilder
+from .scanpy_pl_reduced import ScanpyPlQueryBuilder as ScanpyPlQueryBuilderReduced
+from .scanpy_pp_reduced import ScanpyPpQueryBuilder as ScanpyPpQueryBuilderReduced
 from .scanpy_tl import ScanpyTlQueryBuilder
 
 __all__ = [
@@ -37,6 +39,7 @@ __all__ = [
     "OncoKBQueryBuilder",
     "ScanpyPlQueryBuilder",
     "ScanpyPlQueryBuilderReduced",
+    "ScanpyPpQueryBuilderReduced",
     "ScanpyTlQueryBuilder",
     "format_as_python_call",
     "format_as_rest_call",
