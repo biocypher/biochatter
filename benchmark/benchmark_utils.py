722c979a65ca3ce7df7a1097a54d467e01ac3d79
HansJarchow <hansjarchow@gmail.com>
2025-01-13 19:36:01 +0100

    add judgement logic

--- benchmark/benchmark_utils.py
+++ benchmark/benchmark_utils.py
@@ -1,5 +1,7 @@
 import re
 from datetime import datetime
+import os
+import yaml
 
 import importlib_metadata
 import pandas as pd
@@ -31,9 +33,12 @@ def benchmark_already_executed(
             run, False otherwise
 
     """
-    if "responses" in task:
+    if "judgement" in task:
+        task_results = return_or_create_judge_file(task, model_name)
+        return False
+    elif "generation_plain" in task:
         task_results = return_or_create_response_file(task, model_name)
-    elif "rag" in task:
+    elif "generation_rag" in task:
         task_results = return_or_create_rag_response_file(task, model_name)
     else:
         task_results = return_or_create_result_file(task)
@@ -227,7 +232,6 @@ def return_or_create_rag_response_file(task: str, model: str):
                 "individual",
                 "prompt",
                 "response",
-                "contexts",
                 "expected_answer",
                 "summary",
                 "key_words",
@@ -280,7 +284,7 @@ def get_response_mode_file_path(task: str, model: str) -> str:
     Returns:
         str: The path to the response mode file
     """
-    return f"benchmark/results/{task}_{model}_response.csv"
+    return f"benchmark/LLM_as_a_Judge/responses/{task}_{model}_response.csv"
 
 def get_rag_response_mode_file_path(task: str, model: str) -> str:
     """
@@ -293,7 +297,7 @@ def get_rag_response_mode_file_path(task: str, model: str) -> str:
     Returns:
         str: The path to the response mode file
     """
-    return f"benchmark/results/{task}_{model}_rag_response.csv"
+    return f"benchmark/LLM_as_a_Judge/responses/{task}_{model}_rag_response.csv"
 
 def write_results_to_file(
     model_name: str,
@@ -607,3 +611,235 @@ def get_result_file_path(file_name: str) -> str:
 
     """
     return f"benchmark/results/{file_name}.csv"
+
+
+###########################
+#   FUNCTIONS JUDGEMENT   #
+###########################
+
+def list_files(path: str):
+    """
+    Lists all non-hidden files in a specified directory.
+
+    This function iterates through the contents of a given directory and collects 
+    the names of files that are not hidden (files that do not start with a dot `.`).
+
+    Args:
+        path (str): The path to the directory to be scanned for files.
+
+    Returns:
+        list: A list of filenames (str) in the specified directory, excluding hidden files.
+    """
+
+    files = []
+    for file in os.listdir(path):
+        if not file.startswith("."):
+            files.append(file)
+    return files
+
+def read_file(path: str):
+    """
+    Reads a CSV file and returns its contents as a Pandas DataFrame.
+
+    This function uses Pandas to load the contents of a specified CSV file into 
+    a DataFrame for further analysis or processing.
+
+    Args:
+        path (str): The path to the CSV file to be read.
+
+    Returns:
+        pd.DataFrame: A DataFrame containing the contents of the CSV file.
+    """
+
+    df = pd.read_csv(path)
+    return df
+
+def return_or_create_judge_file(task: str, evaluated_model: str):
+    """
+    Creates a judgement DataFrame and saves it as a CSV file.
+    
+    Args:
+        judged_model (str): Name of the judged model.
+        evaluated_model (str): Name of the evaluated model.
+        metric (str): Name of the metric.
+    
+    Returns:
+        df: A dataframe.
+    """
+
+    path = f"./benchmark/LLM_as_a_Judge/judgement/{task}_{evaluated_model}.csv"
+    try:
+        results = pd.read_csv(path)
+    except (pd.errors.EmptyDataError, FileNotFoundError):
+        results = {
+            "model_name": [],
+            "evaluated_model": [],
+            "iterations": [],
+            "metric": [],
+            "case_id": [],
+            "subtask": [],
+            "individual": [],
+            "md5_hash": [],
+            "prompt": [],
+            "system_prompt": [],
+            "prompt_type": [],
+            "is_appendix": [],
+            "responses": [],
+            "expected_answer": [],
+            "rating": [],
+        }
+        results = pd.DataFrame(results)
+        results.to_csv(path, index = False)
+    return results
+
+def write_judgement_to_file(
+    judge_model: str,
+    evaluated_model: str,
+    iterations: str,
+    metric: str,
+    case_id: str,
+    subtask: str,
+    individual: str,
+    md5_hash: str,
+    prompt: str,
+    system_prompt: str,
+    prompt_type: str,
+    is_appendix: str,
+    responses: list,
+    expected_answer: str,
+    rating: str,
+    path: str,
+):
+    """
+    Appends a new judgement entry to an existing CSV file and saves the updated results.
+
+    This function reads an existing CSV file containing judgement results, adds a new 
+    row with the provided data, sorts the results by specified columns, and writes the 
+    updated results back to the file.
+
+    Args:
+        judge_model (str): The name of the model performing the judgement.
+        evaluated_model (str): The name of the model being evaluated.
+        iterations (str): The number of iterations performed for the judgement.
+        metric (str): The metric used for evaluation.
+        subtask (str): The subtask or specific scenario being judged.
+        individual (str): Metadata, specific for the longevity and geriatrics benchmark example.
+        md5_hash (str): A unique hash representing the input data for the judgement.
+        prompt (str): The prompt used to query the evaluated model.
+        responses (list): The model's responses to the prompt.
+        expected_answer (str): The expected answer for the task.
+        rating (str): The rating or score assigned to the response.
+        path (str): Path to the CSV file where the judgement results are stored.
+    """
+
+    results = pd.read_csv(path, header = 0)
+    new_row = pd.DataFrame([
+        [judge_model, evaluated_model, iterations, metric, case_id, subtask, individual, 
+         md5_hash, prompt, system_prompt, prompt_type, is_appendix, responses, expected_answer, rating]
+    ], columns = results.columns)
+
+    results = pd.concat([results, new_row], ignore_index = True).sort_values(
+        by = ["model_name", "metric"],
+    )
+    results.to_csv(path, index = False)
+
+def read_prompts(path: str):
+    """
+    Reads and loads prompt configurations from a specified `yaml` file.
+
+    This function opens a a specified `yaml` file at the specified path, reads its content, 
+    and extracts the "prompts" section into a Python dictionary.
+
+    Args:
+        path (str): The path to the `yaml` file containing the prompt configurations.
+
+    Returns:
+        dict: A dictionary containing the "prompts" section of the `yaml` file.
+    """
+
+    with open(path, "r") as file:
+        tasks = yaml.safe_load(file)["prompts"]
+    return tasks
+
+def get_prompt_binary(path: str):
+    """
+    Retrieves a specific prompt based on a given metric from the `prompt.yaml` configuration file.
+
+    This function reads a set of prompts from the `prompt.yaml` configuration file and extracts the prompt 
+    that corresponds to the provided metric.
+
+    Args:
+        path (str): The path to the file containing the prompt configurations.
+        metric (str): The evaluation metric for which the prompt is to be retrieved.
+
+    Returns:
+        str: The prompt string associated with the specified metric.
+    """
+
+    prompts = read_prompts(path)
+    # prompt = prompts[metric]["prompt"]
+    return prompts
+
+def load_judgement_dataset(path: str):
+    """
+    Loads and processes judgement dataset files from a specified directory.
+
+    This function scans a directory for files, reads them as CSV files, 
+    concatenates the contents if there are multiple files, and formats 
+    the data into a dictionary.
+
+    Args:
+        path (str): Path to the directory containing judgement (response) files.
+
+    Returns:
+        dict: A dictionary with a single key `"judgement"` containing a list of records 
+        from the loaded CSV files.
+
+    Raises:
+        ValueError: If no files are found in the specified directory.
+    """
+
+    files = list_files(path)
+    if not files:
+        raise ValueError(f"No files found in directory: {path}")
+
+    latest_file = [max([f"{path}/{file}" for file in files], key = os.path.getmtime)]
+
+    dfs = []
+    for file in latest_file: # or files if each response file should be judged
+        # file_path = os.path.join(path, file)
+        try:
+            df = pd.read_csv(file)
+            dfs.append(df)
+        except Exception as err:
+            print(f"Error reading {file}: {err}")
+            continue
+    
+    if len(dfs) > 1:
+        concatenated_dfs = pd.concat(dfs, ignore_index = True)
+    else:
+        concatenated_dfs = dfs[0]
+
+    result_data = concatenated_dfs.to_dict(orient = "records")
+    data_dict = {
+        "judgement": result_data
+    }
+
+    return data_dict
+
+def get_judgement_dataset(path: str):
+    """
+    Retrieves judgement dataset from the specified directory.
+
+    This function loads the judgement dataset by transferring the task to 
+    `load_judgement_dataset` and returns the resulting data.
+
+    Args:
+        path (str): Path to the directory containing judgement files.
+
+    Returns:
+        dict: A dictionary containing the loaded judgement data.
+    """
+
+    test_data = load_judgement_dataset(path)
+    return test_data
