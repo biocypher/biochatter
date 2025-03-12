d98e1aecad98940b2d4548281b3a335fcbdb566d
Francesco Carli <francesco.carli94@gmail.com>
2025-03-12 00:03:54 +0000

    Adding support to gemini-2.0-flash model (#276)
    * Adding support to gemini-2.0-flash model
    
    * Re-organize test structure
    
    * Add GeminiConversation tests
    
    * Add back skip mark to API tests
    
    * line length
    
    * google style key
    
    * fix docstring
    
    * basic dev test for checking functionality
    
    ---------
    
    Co-authored-by: slobentanzer <sebastian.lobentanzer@gmail.com>

---
+++ test/test_llm_connect/test_anthropic_llm_connect.py
@@ -0,0 +1,20 @@
+import os
+
+import pytest
+
+from biochatter.llm_connect import (
+    AnthropicConversation,
+)
+
+
+@pytest.mark.skip(reason="Live test for development purposes")
+def test_anthropic():
+    conv = AnthropicConversation(
+        model_name="claude-3-5-sonnet-20240620",
+        prompts={},
+        split_correction=False,
+    )
+    assert conv.set_api_key(
+        api_key=os.getenv("ANTHROPIC_API_KEY"),
+        user="test_user",
+    )
