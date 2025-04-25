bede90e675efa76a418c56db4e145b8d902c7540
Francesco Carli <francesco.carli94@gmail.com>
2025-04-25 11:56:55 +0100

    Fix import for Bloom conversation

--- biochatter/llm_connect/__init__.py
+++ biochatter/llm_connect/__init__.py
@@ -5,7 +5,7 @@ from biochatter.llm_connect.azure import AzureGptConversation
 from biochatter.llm_connect.conversation import Conversation
 from biochatter.llm_connect.gemini import GeminiConversation
 from biochatter.llm_connect.langchain import LangChainConversation
-from biochatter.llm_connect.misc import WasmConversation
+from biochatter.llm_connect.misc import BloomConversation, WasmConversation
 from biochatter.llm_connect.ollama import OllamaConversation
 from biochatter.llm_connect.openai import GptConversation
 from biochatter.llm_connect.xinference import XinferenceConversation
@@ -13,6 +13,7 @@ from biochatter.llm_connect.xinference import XinferenceConversation
 __all__ = [
     "AnthropicConversation",
     "AzureGptConversation",
+    "BloomConversation",
     "Conversation",
     "GeminiConversation",
     "GptConversation",
