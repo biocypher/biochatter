42fba98a5d4d5d21d295a3dda4745ab04fc80d71
Francesco Carli <francesco.carli94@gmail.com>
2025-04-26 22:08:52 +0100

    Define state of the langgraph agent

--- biochatter/llm_connect/langgraph/state.py
+++ biochatter/llm_connect/langgraph/state.py
@@ -8,6 +8,7 @@ from typing_extensions import TypedDict
 
 class ConversationState(TypedDict):
     messages: Annotated[list, operator.add]
+    iteration: int = 0
     current_question: str | None
     search_queries: list[str]
     tool_calls: list
