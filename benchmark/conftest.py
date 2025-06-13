00c9ee64daf8fda6f1d6ea9df0515d7a53a954bb
HansJarchow <hansjarchow@gmail.com>
2025-06-13 15:17:39 +0200

    add models

--- benchmark/conftest.py
+++ benchmark/conftest.py
@@ -35,6 +35,9 @@ OPENAI_MODEL_NAMES = [
     # "gpt-4o-2024-08-06",
     # "gpt-4o-2024-11-20",
     # "gpt-4o-mini-2024-07-18",
+    # "o1-preview-2024-09-12",
+    # "o1-mini-2024-09-12",
+    # "o3-mini",
 ]
 
 GROQ_MODEL_NAMES = [
