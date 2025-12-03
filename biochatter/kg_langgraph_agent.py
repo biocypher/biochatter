80631957aa4eab5d9aadc146ca0a66a7a52c963e
Harmen <hdraisma@users.noreply.github.com>
2025-12-03 18:47:32 +0000

    Update kg_langgraph_agent.py: prevent LangChainDeprecationWarning from being issued
    Prevent the following "LangChainDeprecationWarning" from being issued:
    ====
    [~]/biochatter/biochatter/database_agent.py:8: LangChainDeprecationWarning: As of langchain-core 0.3.0, LangChain uses pydantic v2 internally. The langchain_core.pydantic_v1 module was a compatibility shim for pydantic v1, and should no longer be used. Please update the code to import from Pydantic directly.
    
    For example, replace imports like: `from langchain_core.pydantic_v1 import BaseModel`
    with: `from pydantic import BaseModel`
    or the v1 compatibility namespace if you are working in a code base that has not been fully upgraded to pydantic 2 yet.         from pydantic.v1 import BaseModel
    
      from .kg_langgraph_agent import KGQueryReflexionAgent
    ====

--- biochatter/kg_langgraph_agent.py
+++ biochatter/kg_langgraph_agent.py
@@ -10,7 +10,7 @@ from langchain.output_parsers.openai_tools import (
 )
 from langchain_core.messages import AIMessage, BaseMessage, ToolMessage
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain_core.pydantic_v1 import BaseModel, Field
+from pydantic import BaseModel, Field
 from langchain_openai import ChatOpenAI
 from langgraph.graph import END
 
